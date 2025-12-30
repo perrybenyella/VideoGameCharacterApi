59e7e0416b1246ac311e60b36b3591d3372fd786
Perry Benyella <pebenyella@microsoft.com>
2025-12-30 09:52:12 -0800

    Add ConnectionCheckResponse DTO for DB connection status
    Introduced the VideoGameCharacterApi.Dtos namespace and added the ConnectionCheckResponse class. This DTO includes EfCoreCanConnect, RawCanOpen, and Error properties to represent database connection status and errors.

---
+++ VideoGameCharacterApi/Dtos/ConnectionCheckResponse.cs
@@ -0,0 +1,10 @@
+﻿namespace VideoGameCharacterApi.Dtos
+{
+    public class ConnectionCheckResponse
+    {
+        public bool EfCoreCanConnect { get; init; }
+        public bool RawCanOpen { get; init; }
+        public string? Error { get; init; }
+
+    }
+}
